ae84030c8bdfc8195b1792208d21cadc4f357b4c
Tom Peney <tompeney@hotmail.co.uk>
2021-03-06 02:17:58 +0000

    Started work on track length normalization

--- pipe_test.py
+++ pipe_test.py
@@ -86,6 +86,17 @@ track1 = track_info.pop(0)
 track2 = track_info.pop(0)
 
 
+track1_end = float(track1['end'])
+track2_end = float(track2['end'])
+
+timeshift = ((track2_end - track1_end) / track1_end) * 100
+
+
+print(timeshift)
+
+do_command(f'ChangeSpeed: Percentage={-(timeshift)}') # Still not 100% accurate?
+
+
 """
 Scratchpad
 
