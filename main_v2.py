ae55cac84ec29d273d5caefb35ddd8cd857c1ad3
Tom Peney <tompeney@hotmail.co.uk>
2021-03-11 01:45:12 +0000

    Initial v2 commit

--- main_v2.py
+++ main_v2.py
@@ -5,24 +5,26 @@ class Project:
     def __init__(self):
         self.tracks = {}
 
+    def select_track(self, index, mode='Set'):
+        pipe.do_command(f'SelectTracks: Mode={mode} Track={index} TrackCount=1')
+
+
     def import_track(self, file, trunc_silence=True):
-        track_id = len(self.tracks)
+        track_index = len(self.tracks)
         pipe.do_command(f'Import2: Filename="{file}"')
         json_info = json.loads(pipe.do_command("GetInfo: Type=Tracks").replace("BatchCommand finished: OK", ''))
         dic = json_info.pop(track_id)
 
-        self.tracks[track_id] = Track(dic, track_id)
+        self.tracks[track_index] = Track(dic,)
         
-
+        self.select_track(track_index)
+        pipe.do_command(f'SetTrackStatus: Name={self.name}')
 
 
 class Track:
-    def __init__(self, track_info, id, name=track_info['name']):
-        self.length = float(track_info_dict['end'])
-        self.id = id    
+    def __init__(self, track_info, name=f"{name}{str(file_number).zfill(3)}"):
+        self.length = float(track_info['end'])
         self.name = name
         pipe.do_command(f'SelectTracks: Mode=Set Track={self.id} TrackCount=1')
         pipe.do_command(f'SetTrackStatus: Name={self.name}')
         self.data = [self.name, self.id, self.length]
-
-    def rename(self, new_name):
