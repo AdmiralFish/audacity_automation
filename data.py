6a281ffe81a8035729036c7b47d1d87197c08760
Tom Peney <tompeney@hotmail.co.uk>
2021-03-11 04:39:24 +0000

    Import file paths fixed, added error handling for empty input / non-empty export

--- data.py
+++ data.py
@@ -5,29 +5,28 @@ def file_list(directory):
     # Returns a list of all WAV files in a given directory. 
     return [file.replace("\\", "/") for file in glob.glob(f"{directory}/*.wav")]
 
-
-# Creates a list of all WAV files for set_a and set_b. 
+# Links to input folder.
 cwd = os.getcwd()
+input_dir = cwd + r"\input"
 
-# input_sets = 
+# List of sub-directory names in 'input'.
+input_sets = [dirs for root, dirs, files in os.walk(input_dir)][0]
+if len(input_sets) < 2:
+    raise Exception("ERROR: Did not detect 2 or more input folders.")
 
-set_a = file_list(cwd + r"\set_a")
-set_b = file_list(cwd + r"\set_b")
-
-white_noise_path = cwd + r"\media\white_noise.wav"
-
+# Stores dict of sub-dict name: file path.
+input_files = {}
+for set_name in input_sets:
+    input_files[set_name] = file_list(f"{input_dir}/{set_name}")
+# Adds path to white noise file into dict.
+input_files["white noise"] = cwd + r"\media\white_noise.wav"
 
 # Labels for naming combined track. 
-set_a_names = [f"set_A{str(i).zfill(3)}" for i in range(1, len(set_a)+1)]
-set_b_names = [f"set_B{str(i).zfill(3)}" for i in range(1, len(set_b)+1)]
-
+# set_a_names = [f"set_A{str(i).zfill(3)}" for i in range(1, len(set_a)+1)]
+# set_b_names = [f"set_B{str(i).zfill(3)}" for i in range(1, len(set_b)+1)]
 
 # Check to see if export directoires are empty. 
-exp_files = file_list(cwd + r"exported_files")
-exp_proj = file_list(cwd + r"exported_projects")
-if exp_files or exp_files == []:
-    export_dir_empty = True
-
-
-input_dir = cwd+r"\input"
-input_names = [dirs[0] for root, dirs, files in os.walk(input_dir)]
+exp_files = file_list(cwd + r"\output\merged_audio")
+exp_proj = file_list(cwd + r"\output\project_data")
+if exp_files != [] or exp_proj != []:
+    raise Exception("One or both output folders not empty.")
